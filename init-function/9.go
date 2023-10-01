4cda09b9cf92c601dadb0d67676514bc610077cc
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2023-10-01 20:14:31 +0530

    new file:   init-function/main.go

---
+++ init-function/9.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func init() {
+	fmt.Println("Init from 9.go")
+}
---
+++ init-function/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func init() {
+	fmt.Println("Init from main.go")
+}
+
+func main() {
+	fmt.Println("Hello, World!")
+}
